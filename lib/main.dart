b65d7698d090a2280d026430f589acdd04a0df2b
Yesra Fatima <147398141+yesra29@users.noreply.github.com>
2026-05-18 16:59:53 +0530

    #COMMENT riverpod provider added in the main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_knp_mobile_app_v2/app/environments/env.dart';
 import 'package:flutter_knp_mobile_app_v2/modules/auth/presentation/screens/screen1.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-Future<void>  main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName:".env");
-  await Supabase.initialize(
-    url: Env.supabaseUrl,
-    anonKey:Env.supabaseAnonKey,
-  );
-  print("Supabase Initialized Successfully");
-  runApp(const MyApp());
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseAnonKey);
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-
-
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Kanpur App',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const Screen1(),
     );
   }
