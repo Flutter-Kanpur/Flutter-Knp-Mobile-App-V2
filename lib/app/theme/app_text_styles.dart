aec39cb83bb6cd6c4779f5a9baac7b323cd6c6e6
Yesra Fatima <147398141+yesra29@users.noreply.github.com>
2026-05-20 11:11:59 +0530

    FKP-99 #COMMENT added icons in assets and dependencies in pubspec.yaml, edited hi.json and en.json, added theming

---
+++ lib/app/theme/app_text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+TextStyle textStyle_24RegularWhite() {
+  return TextStyle(
+    fontFamily: 'ProductSans',
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w400,
+    color: const Color(0xFFFFFFFF),
+    height: 1.0,
+  );
+}
